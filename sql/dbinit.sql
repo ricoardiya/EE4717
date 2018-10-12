64edc86a98a00ea71532fd0f81e818676f8aa84b
theorizchycleven <theorizchy@gmail.com>
2018-10-12 12:36:22 +0800

    add Customer and Order Table

--- sql/dbinit.sql
+++ sql/dbinit.sql
@@ -10,6 +10,8 @@ Run this file to intialize database
 DROP TABLE IF EXISTS pictures;
 DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS orders;
 
 -- Initialize products table
 
@@ -43,3 +45,32 @@ CREATE TABLE pictures (
   FOREIGN KEY (productID) REFERENCES products(id),
   PRIMARY KEY (id)
 )
+
+
+-- Initialize customers table
+
+CREATE TABLE customers (
+  `id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `zipCode` int(6) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(8) NOT NULL,
+  PRIMARY KEY (id)
+)
+-- Initialize orders table
+
+CREATE TABLE orders (
+  `id` int(10) NOT NULL,
+  `customerID` INT(10) NOT NULL,
+  `dateOrder` DATE NOT NULL,
+  `productID` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `size` int(2) NOT NULL,  
+  PRIMARY KEY (id)
+  FOREIGN KEY (productID) REFERENCES products(id),
+  FOREIGN KEY (customerID) REFERENCES customers(id),
+)
+
+-- 1 customer punya banyak order, 1 order punya 1 customer
+-- 1 order punya 1 product , tapi sebaliknya tidak

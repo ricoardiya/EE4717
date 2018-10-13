d96476dc766806a98ec9d5bfb50a9b5cce59e036
Henrikus Theorizchy Cleven <theorizchy@gmail.com>
2018-10-13 16:12:28 +0800

    test 1

--- php/nav.php
+++ php/nav.php
@@ -4,6 +4,6 @@
       <li><a href="#" class="option">Men</a></li>
       <li><a href="#" class="logo">SE PA TU</a></li>
       <li><a href="#" class="option">Women</a></li>
-    </ul> 
+    </ul>
   </header>
 </section>

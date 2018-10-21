43aa2fd2fd5dbf0be20fd22c9b7d1ca279c47361
theorizchy <HENR0011@e.ntu.edu.sg>
2018-10-22 00:26:30 +0800

    skeleton product card

--- men-catalog/index.php
+++ men-catalog/index.php
@@ -22,14 +22,20 @@
             // output data of each row
             while($row = mysqli_fetch_assoc($result)) {
               echo '
+
                 <div class="card">
                   <img src="../assets/pictures/men/boots-captoe-brogues-espresso-crazy-horse-leather/boots-captoe-brogues-espresso-crazy-horse-leather-1.jpg" alt="Avatar" style="width:100%">
                   <div class="container">
                     <h4><b>'.ucwords($row['name']).'</b></h4>
                     <p>'.ucwords($row['color']).'</p>
                     <p>'.$row['desc'].'</p>
+                    <form action="">
+                      <button type="submit">Add To Cart</button>
+                    </form>
                   </div>
-                </div>
+                  </div>
+                <br><br><br>
+
                 ';
               }
           }

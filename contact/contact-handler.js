9a7f42a72c1232727f8bbfe102efb160ac75b29e
Rico Ardiya <ricoardiya@yahoo.com>
2018-10-31 22:24:07 +0800

    Add javascript handler for contact us validation

--- contact/contact-handler.js
+++ contact/contact-handler.js
@@ -2,6 +2,6 @@ var firstname = document.getElementById("fname");
 var lastname = document.getElementById("lname");
 var email = document.getElementById("email");
 
-firstname.addEventListener("change", chkName, false);
-lastname.addEventListener("change", chkName, false);
-email.addEventListener("change", chkEmail, false);
+firstname.addEventListener("change", checkFirstName, false);
+lastname.addEventListener("change", checkLastName, false);
+email.addEventListener("change", checkEmail, false);

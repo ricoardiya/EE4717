8a7f0518914dd4ccb9bca9e8fdf7cd10339733e8
Rico Ardiya <ricoardiya@yahoo.com>
2018-11-01 16:37:31 +0800

    Sign out admin

--- admin/index.php
+++ admin/index.php
@@ -27,18 +27,31 @@
       </header>
     </section>
     <div class="content-wrapper">
-      <div class="index-content">
+      <div class="admin-content">
+        <div class="admin-nav">
+          <ul>
+            <li><a class="active" href=<?php echo $root_path .'/admin' ?>>Inventory</a></li>
+            <li><a href=<?php echo $root_path .'/admin/price-list.php' ?>>Price list</a></li>
+            <li><a href=<?php echo $root_path .'/admin/sales.php' ?>>Sales</a></li>
+            <li><a href=<?php echo $root_path .'/admin/admin-signout.php' ?>>SIGN OUT</a></li>
+          </ul>
 
-      </div>
-      <section>
-        <div class="index-footer-wrapper">
-          <div class="index-footer">
-            <div class="index-footer-left">
-              &copy; Sepatu Project 2018
-            </div>
+          <div style="margin-left:20%;padding:1px 16px;height:1000px;">
+            <h2>Fixed Full-height Side Nav</h2>
+            <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
+            <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
+            <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
+            <p>Some text..</p>
           </div>
         </div>
-      </section>
+      </div>
+      <?php include '../common/footer.php'?>
     </div>
   </body>
 </html>

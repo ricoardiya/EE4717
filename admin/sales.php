b492741486cfe122c41c310baf3b58aaf46c02bd
Rico Ardiya <ricoardiya@yahoo.com>
2018-11-02 15:08:38 +0800

    Add total revenue

--- admin/sales.php
+++ admin/sales.php
@@ -3,6 +3,7 @@
   <?php
     include '../head.php';
     include '../path.php';
+    include '../dbconn.php';
   ?>
   <link rel="stylesheet" type="text/css" href="admin.css">
   <body>
@@ -38,6 +39,47 @@
 
           <div style="margin-left:20%;padding:1px 16px;">
             <h2>Sales</h2>
+            <?php
+              $quantity_query = "SELECT products.name, SUM(orders.quantity) AS total_quantity
+                                  FROM orders
+                                  JOIN products ON orders.productID = products.id
+                                  GROUP BY productID";
+
+              $quantity_result = mysqli_query($conn, $quantity_query);
+
+              echo '<table id="inventory">';
+              echo '
+                      <tr>
+                        <th>Product name</th>
+                        <th>Total quantity sold</th>
+                      </tr>
+                  ';
+              if(mysqli_num_rows($quantity_result) > 0) {
+                while($quantity_row = mysqli_fetch_assoc($quantity_result)) {
+                  echo '
+                    <tr>
+                      <td>' . ucwords($quantity_row['name']) . '</td>
+                      <td>' . $quantity_row['total_quantity'] . '</td>
+                    </tr>
+                  ';
+                }
+              }
+
+              echo '</table>';
+
+              $total_revenue_query = "SELECT SUM(totalPrice) AS total_revenue FROM transactions";
+
+              $total_revenue_result = mysqli_query($conn, $total_revenue_query);
+
+              echo '<br><br>';
+
+              if(mysqli_num_rows($total_revenue_result) > 0) {
+                $total_revenue_row = mysqli_fetch_assoc($total_revenue_result);
+              }
+              echo '<h2>Total revenue : $ ' . $total_revenue_row['total_revenue'] . '</h2>';
+
+              echo '<br>';
+            ?>
           </div>
         </div>
       </div>
